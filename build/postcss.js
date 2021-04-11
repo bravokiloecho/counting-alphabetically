bded45bf2d910483b6db02162a8f7d0684b707f2
Benjamin E <ben@elwyn.co>
2021-04-11 09:10:33 +0100

    start postcss build

---
+++ build/postcss.js
@@ -0,0 +1,24 @@
+const autoprefixer = require('autoprefixer');
+const postcss = require('postcss');
+const precss = require('precss');
+const fs = require('fs');
+
+const srcDir = 'src';
+const destDir = 'public';
+const filename = 'style.css';
+
+const plugins = [
+  precss,
+  autoprefixer,
+];
+
+fs.readFile(`${srcDir}/${filename}`, (err, css) => {
+  postcss(plugins)
+    .process(css, { from: `${srcDir}/${filename}`, to: `${destDir}/${filename}` })
+    .then((result) => {
+      fs.writeFile(`${destDir}/${filename}`, result.css, () => true);
+      if (result.map) {
+        fs.writeFile(`${destDir}/${filename}.map`, result.map.toString(), () => true);
+      }
+    });
+});
